c93aa6ff2312084db62c7ddd4c5f26bf4436131a
drandredev <augusto.delgado@tecsup.edu.pe>
2022-06-02 13:30:46 -0500

    fix(lint for cypress files)

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-        baseUrl: 'http://localhost:5000',
-        supportFile: false,
-        video: false
-        
+    baseUrl: 'http://localhost:5000',
+    supportFile: false,
+    video: false
   }
 })
